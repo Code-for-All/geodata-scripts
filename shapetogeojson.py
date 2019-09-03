910b812bfdae1e098cdac3c9a35ba9a13bdb8850
milovanderlinden <milo@dogodigi.net>
2019-09-03 19:38:33 +0200

    Raw shape2geojson python script

---
+++ shapetogeojson.py
@@ -0,0 +1,71 @@
+import os
+import urllib.request
+import zipfile
+import shapefile
+import datetime
+
+
+def myconverter(o):
+  """
+  DateTime objects need to be converted otherwise json will throw an error 
+  See: https://code-maven.com/serialize-datetime-object-as-json-in-python
+  """
+
+  if isinstance(o, datetime.datetime):
+        return o.__str__()
+
+
+def shape2geojson(filename):
+  """
+  When the zip is downloaded and converted to shape, 
+  we pass the filename to shape2geojson
+  to create a geojson file.
+  We can do a lot of extra's here, like split the geojson to multiple files.
+  """
+
+  # read the shapefile
+  reader = shapefile.Reader(filename)
+  fields = reader.fields[1:]
+  field_names = [field[0] for field in fields]
+  buffer = []
+  for sr in reader.shapeRecords():
+    atr = dict(zip(field_names, sr.record))
+    geom = sr.shape.__geo_interface__
+    buffer.append(dict(type="Feature", \
+        geometry=geom, properties=atr))
+
+  # write the GeoJSON file
+  from json import dumps
+  geojson = open(filename + ".geojson", "w")
+  geojson.write(dumps({"type": "FeatureCollection",\
+    "features": buffer}, indent=2, default=myconverter) + "\n")
+  geojson.close()
+
+def getunzipped(theurl, thedir):
+  """
+  Download a file from a url and unzip it
+  """
+
+  name = os.path.join(thedir, 'temp.zip')
+  try:
+    name, hdrs = urllib.request.urlretrieve(theurl, name)
+  except IOError as e:
+    print ("Can't retrieve %r to %r: %s" % (theurl, thedir, e))
+    return
+  try:
+    z = zipfile.ZipFile(name)
+  except zipfile.error as e:
+    print ("Bad zipfile (from %r): %s" % (theurl, e))
+    return
+  z.extractall(thedir)
+  z.close()
+  os.unlink(name)
+
+def main():
+  """Main - program execute"""
+  getunzipped('http://terrabrasilis.dpi.inpe.br/download/deter-amz/deter-amz_all.zip', './data') 
+  shape2geojson("./data/deter_all.shp")
+
+if __name__ == '__main__':
+  main()
+

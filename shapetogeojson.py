469b645273ee35462391cece33ca896f372c0757
milovanderlinden <milo@dogodigi.net>
2019-09-03 20:42:07 +0200

    write geojson files per month

--- shapetogeojson.py
+++ shapetogeojson.py
@@ -3,7 +3,7 @@ import urllib.request
 import zipfile
 import shapefile
 import datetime
-
+from json import dumps
 
 def myconverter(o):
   """
@@ -11,9 +11,8 @@ def myconverter(o):
   See: https://code-maven.com/serialize-datetime-object-as-json-in-python
   """
 
-  if isinstance(o, datetime.datetime):
-        return o.__str__()
-
+  if isinstance(o, datetime.date):
+        return o.strftime('%Y-%m-%d')
 
 def shape2geojson(filename):
   """
@@ -27,19 +26,32 @@ def shape2geojson(filename):
   reader = shapefile.Reader(filename)
   fields = reader.fields[1:]
   field_names = [field[0] for field in fields]
-  buffer = []
+  months = []
+  
+  for sr in reader.shapeRecords():
+    try:
+      i = months.index(sr.record[3].strftime('%Y%m'))
+    except ValueError:
+      months.append(sr.record[3].strftime('%Y%m'))
+
+  monthsdict = dict()
+  months.sort() 
+  for m in months:
+    monthsdict.update({m: []})
+
   for sr in reader.shapeRecords():
     atr = dict(zip(field_names, sr.record))
+    m = sr.record[3].strftime('%Y%m')
     geom = sr.shape.__geo_interface__
-    buffer.append(dict(type="Feature", \
+    monthsdict[m].append(dict(type="Feature", \
         geometry=geom, properties=atr))
 
   # write the GeoJSON file
-  from json import dumps
-  geojson = open(filename + ".geojson", "w")
-  geojson.write(dumps({"type": "FeatureCollection",\
-    "features": buffer}, indent=2, default=myconverter) + "\n")
-  geojson.close()
+  for key in monthsdict:
+    geojson = open(filename + "-" + key + ".geojson", "w")
+    geojson.write(dumps({"type": "FeatureCollection",\
+      "features": monthsdict[key]}, indent=2, default=myconverter) + "\n")
+    geojson.close()
 
 def getunzipped(theurl, thedir):
   """
@@ -63,7 +75,7 @@ def getunzipped(theurl, thedir):
 
 def main():
   """Main - program execute"""
-  getunzipped('http://terrabrasilis.dpi.inpe.br/download/deter-amz/deter-amz_all.zip', './data') 
+  #getunzipped('http://terrabrasilis.dpi.inpe.br/download/deter-amz/deter-amz_all.zip', './data') 
   shape2geojson("./data/deter_all.shp")
 
 if __name__ == '__main__':
